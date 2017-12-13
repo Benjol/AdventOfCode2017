a53200b7d3ee72bc1811ddb534684fbd6063c9e2
Benjol <ben.simkins@gmail.com>
2017-12-13 16:58:10 +0100

    Update Day13.fsx
    Add part two

--- Day13.fsx
+++ Day13.fsx
@@ -11,3 +11,17 @@ input.Split([|'\n';'\r'|], System.StringSplitOptions.RemoveEmptyEntries)
     |> Seq.map (fun (depth, range) -> triangle (range - 1) depth, depth * range)
     |> Seq.filter (fun (pos,_) -> pos = 0)
     |> Seq.sumBy snd
+
+//PART TWO:
+let scans = 
+    input.Split([|'\n';'\r'|], System.StringSplitOptions.RemoveEmptyEntries)
+        |> Array.map (fun def -> def.Split ':' |> Array.map int)
+        |> Array.map (fun a -> a.[0], a.[1])
+
+let hits offset =
+    scans
+        |> Array.Parallel.map (fun (depth, range) -> triangle (range - 1) (depth + offset))
+        |> Seq.filter (fun pos -> pos = 0)
+        |> Seq.length
+
+Seq.initInfinite id |> Seq.find (fun t -> hits t = 0)
