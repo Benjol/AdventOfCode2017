e529463b80f221ebacc9f337a42e123fa7fde0cb
Benjol <ben.simkins@gmail.com>
2017-12-13 13:54:56 +0100

    Create Day13.fsx

---
+++ Day13.fsx
@@ -0,0 +1,13 @@
+let input = "0: 3
+1: 2
+4: 4
+6: 4"
+
+let triangle p x = p - abs (x % (2 * p) - p)
+
+input.Split([|'\n';'\r'|], System.StringSplitOptions.RemoveEmptyEntries)
+    |> Seq.map (fun def -> def.Split ':' |> Array.map int)
+    |> Seq.map (fun a -> a.[0], a.[1])
+    |> Seq.map (fun (depth, range) -> triangle (range - 1) depth, depth * range)
+    |> Seq.filter (fun (pos,_) -> pos = 0)
+    |> Seq.sumBy snd
