e769e83b265984cc0327ce5457a679086d1c33a9
Benjol <ben.simkins@gmail.com>
2017-12-18 12:55:29 +0100

    Update Day17.fsx
    Add part 2

--- Day17.fsx
+++ Day17.fsx
@@ -1,12 +1,23 @@
 let steps = 386
-let target = 2017
-let buffer = ResizeArray<int>()
-buffer.Add(0);
-let mutable iteration, pos = 0, 0
-while iteration < target do
-    iteration <- iteration + 1
-    pos <- 1 + (pos + steps) % iteration
-    buffer.Insert(pos, iteration)
+let findAt inserts find =
+    let buffer = ResizeArray<int>()
+    buffer.Add(0);
+    let mutable iteration, pos = 0, 0
+    while iteration < inserts do
+        iteration <- iteration + 1
+        pos <- 1 + (pos + steps) % iteration
+        buffer.Insert(pos, iteration)
+    let offset = buffer |> Seq.findIndex (fun x -> x = find)
+    buffer |> Seq.item (offset + 1)
 
-let offset = buffer |> Seq.findIndex (fun x -> x = target)
-let part1 = buffer |> Seq.item (offset + 1)
+let part1 = findAt 2017 2017
+
+let findAtOne inserts =
+    let mutable iteration, pos, one = 0, 0, 0
+    while iteration < inserts do
+        iteration <- iteration + 1
+        pos <- 1 + (pos + steps) % iteration
+        if pos = 1 then one <- iteration
+    one
+
+let part2 = findAtOne 50000000
