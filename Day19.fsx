2349319a8b67f1c94fba7e30186e5041b827b380
Benjol <ben.simkins@gmail.com>
2017-12-19 12:02:19 +0100

    Update Day19.fsx

--- Day19.fsx
+++ Day19.fsx
@@ -29,24 +29,24 @@ let nextxy (x, y) direction =
     | Left -> x - 1, y
     | Right -> x + 1, y
 
-let rec Walk (x, y) direction collect =
+let rec Walk (x, y) direction collect steps =
     //printfn "%A %A %A %A" x y direction grid.[y].[x]
     match parse grid.[y].[x] with
-    | Continue -> Walk (nextxy (x,y) direction) direction collect
-    | Collect(c) -> Walk (nextxy (x,y) direction) direction (c::collect)
+    | Continue -> Walk (nextxy (x,y) direction) direction collect (steps + 1)
+    | Collect(c) -> Walk (nextxy (x,y) direction) direction (c::collect) (steps + 1)
     | Turn ->  match direction with
                | Up | Down ->
-                                if x = 0 then Walk (1, y) Right collect
-                                elif x = maxx then Walk (maxx - 1, y) Left collect
-                                elif grid.[y].[x + 1] = ' ' then Walk (x - 1, y) Left collect
-                                else Walk (x + 1, y) Right collect
+                                if x = 0 then Walk (1, y) Right collect (steps + 1)
+                                elif x = maxx then Walk (maxx - 1, y) Left collect (steps + 1)
+                                elif grid.[y].[x + 1] = ' ' then Walk (x - 1, y) Left collect (steps + 1)
+                                else Walk (x + 1, y) Right collect (steps + 1)
                | _ ->
-                                if y = 0 then Walk (x, 1) Down collect
-                                elif y = maxy then Walk (x, maxy - 1) Up collect
-                                elif grid.[y + 1].[x] = ' ' then Walk (x, y - 1) Up collect
-                                else Walk (x, y + 1) Down collect
-    | Stop -> List.rev collect
+                                if y = 0 then Walk (x, 1) Down collect (steps + 1)
+                                elif y = maxy then Walk (x, maxy - 1) Up collect (steps + 1)
+                                elif grid.[y + 1].[x] = ' ' then Walk (x, y - 1) Up collect (steps + 1)
+                                else Walk (x, y + 1) Down collect (steps + 1)
+    | Stop -> steps, List.rev collect |> Array.ofList |> System.String
 
 let startx = Array.findIndex (fun c -> c = '|') grid.[0]
 
-Walk (startx, 0) Down []
+Walk (startx, 0) Down [] 0
