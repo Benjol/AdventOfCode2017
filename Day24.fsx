fffdd9876b627ba25017c23f347040ca46de277c
Benjol <ben.simkins@gmail.com>
2018-01-03 11:37:33 +0100

    Create Day24.fsx

---
+++ Day24.fsx
@@ -0,0 +1,33 @@
+let input = @"0/2
+2/2
+2/3
+3/4
+3/5
+0/1
+10/1
+9/10"
+
+type Component =
+    { Id:int; A:int; B:int }
+    member this.AnyPortIs value = this.A = value|| this.B = value
+    member this.Score = this.A + this.B
+    member this.OtherPort p =
+        if this.A = p then
+            this.B
+        elif this.B = p then
+            this.A
+        else
+            failwith "NO MATCH!?"
+
+let components = input.Split([|'\r';'\n'|], System.StringSplitOptions.RemoveEmptyEntries)
+                 |> Array.map (fun c -> c.Split '/')
+                 |> Array.mapi (fun i a -> { Id = i; A = int a.[0]; B = int a.[1] })
+
+let rec build port list remaining =
+    let matches = remaining |> Array.filter (fun (c:Component) -> c.AnyPortIs port)
+                            |> Seq.collect (fun c -> build (c.OtherPort port) (c::list) (remaining |> Array.filter (fun x -> x.Id <> c.Id)) )
+                            |> List.ofSeq
+    list :: matches
+
+build 0 [] components |> List.map (fun l -> l |> List.sumBy (fun c -> c.Score)) |> List.max
+    
